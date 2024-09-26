144c4a19929ab8c7ab6518929f5a7f84454c9bd7
miniani1015 <ytaemin27@gmail.com>
2024-09-27 08:34:05 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 240 ms, Memory: 18024 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		
+		for (int i = 0; i<N; i++) {
+			for (int l = 0; l<=i; l++) {
+				System.out.print("*");	
+			}
+			System.out.println(" ");
+		}
+		
+	}
+
+}
