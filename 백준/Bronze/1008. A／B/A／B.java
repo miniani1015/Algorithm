1510ec55279bc8a68bab2e58af2200f4d6438341
miniani1015 <ytaemin27@gmail.com>
2024-10-01 08:57:46 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17936 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		double A = scanner.nextDouble();
+		double B = scanner.nextDouble();
+		
+		System.out.println(A / B);
+			
+	}
+
+}
