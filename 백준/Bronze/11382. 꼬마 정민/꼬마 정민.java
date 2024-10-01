56a8334635abbba9a7b3ba7d6b97852e4967c9ba
miniani1015 <ytaemin27@gmail.com>
2024-10-02 00:15:36 +0900

    [Bronze V] Title: 꼬마 정민, Time: 176 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		long A = scanner.nextLong();
+		long B = scanner.nextLong();
+		long C = scanner.nextLong();
+
+		System.out.println(A + B + C);
+
+	}
+
+}
