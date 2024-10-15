48a5f32af01c31c9bb1cf44e8353928f9de99dd1
miniani1015 <ytaemin27@gmail.com>
2024-10-16 08:20:07 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		String str = scanner.next();
+		int c = str.charAt(0);
+		System.out.println(c);
+
+	}
+
+}
