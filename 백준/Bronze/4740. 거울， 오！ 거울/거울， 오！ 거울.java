c13fe923ae6535c486051ac05d8805f11fadfe27
miniani1015 <ytaemin27@gmail.com>
2024-11-06 08:34:55 +0900

    [Bronze III] Title: 거울, 오! 거울, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/4740. 거울， 오！ 거울/거울， 오！ 거울.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			String N = sc.nextLine();
+			if (N.equals("***")) break;
+			for (int i=N.length()-1; i>=0; i--) {
+				System.out.print(N.charAt(i));			
+			}
+			System.out.println("");
+			
+		}
+
+	}
+
+}
