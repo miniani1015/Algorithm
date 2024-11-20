91c8e63b38c824087ebcdc5cf46a3d8acf1287e7
miniani1015 <ytaemin27@gmail.com>
2024-11-21 08:50:32 +0900

    [Bronze I] Title: 애너그램, Time: 212 ms, Memory: 18636 KB -BaekjoonHub

---
+++ 백준/Bronze/6996. 애너그램/애너그램.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    private static boolean solveAnagrams(String first, String second ) {
+        char[] a = first.toCharArray();
+        char[] b = second.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numTests = sc.nextInt();
+
+        for (int i = 0; i < numTests; i++) {
+            String first = sc.next().toLowerCase();
+            String second = sc.next().toLowerCase();
+
+            System.out.println(first + " & " + second + " are " + (solveAnagrams(first, second) ? "anagrams." : "NOT anagrams."));
+        }
+    }
+}
