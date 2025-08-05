bc5f89e027a5326f4120d6580853f01b9879a6fb
miniani1015 <ytaemin27@gmail.com>
2025-08-05 21:32:07 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 184 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+        	int M = sc.nextInt();
+        	int F = sc.nextInt();
+        	if (M == 0 && F == 0) {
+        		break;
+        	}
+        	else {
+        		System.out.println(M + F);
+        	}
+        }
+
+    }
+}
